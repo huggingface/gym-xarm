3a72cf0a36c2a9f73ebf1020705ea26f20a37dc0
Simon Alibert <alibert.sim@gmail.com>
2024-04-06 13:00:22 +0200

    Cleanup

--- tests/test_env.py
+++ tests/test_env.py
@@ -15,7 +15,3 @@ import gym_xarm  # noqa: F401
 def test_env(env_task, obs_type):
     env = gym.make(f"gym_xarm/{env_task}", obs_type=obs_type)
     check_env(env.unwrapped)
-
-
-if __name__ == "__main__":
-    test_env("XarmLift-v0", "pixels_agent_pos")
