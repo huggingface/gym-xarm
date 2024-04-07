14875d37f4125423510f848eea7537e6fb5c5bf9
Simon Alibert <alibert.sim@gmail.com>
2024-04-07 14:37:07 +0200

    Add example, Update README

---
+++ example.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+import gym_xarm
+
+env = gym.make("gym_xarm/XarmLift-v0", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    image = env.render()
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
